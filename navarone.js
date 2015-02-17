10ea6adbcf0e38fe29f280f1d00bce67820c6256
mandeeeh <mipodmy@gmail.com>
2015-02-17 23:31:56 +0100

    added pics

--- navarone.js
+++
@@ -1,7 +0,0 @@
-function parallax() {
-	var prlx_layer_1 = document.getElementById('prlx_layer_1');
-	var prlx_layer_2 = document.getElementById('prlx_layer_2');
-	prlx_layer_1.style.top = (window.pageYOffset / 4)+'px';
-	prlx_layer_2.style.top = -(window.pageYOffset / 8)+'px';
-}
-window.addEventListener("scroll", parallax, false);
